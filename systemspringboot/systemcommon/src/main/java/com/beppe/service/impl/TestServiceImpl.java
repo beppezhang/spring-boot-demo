3f6d556d26c3265f1f81a00eb65cd56ccea708a8
beppezhang <beppezhang@163.com>
2018-04-04 00:34:46 +0800

    提交了代码

---
+++ systemspringboot/systemcommon/src/main/java/com/beppe/service/impl/TestServiceImpl.java
@@ -0,0 +1,11 @@
+package com.beppe.service.impl;
+
+import org.junit.Test;
+
+public class TestServiceImpl {
+
+    @Test
+    public  void test1(){
+        System.out.println("this is the test1");
+    }
+}
