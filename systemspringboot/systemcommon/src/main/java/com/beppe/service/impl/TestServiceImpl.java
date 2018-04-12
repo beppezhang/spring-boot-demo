d3c14218721ad650cbe4de40cf54b23025d78ea1
beppezhang <beppezhang@163.com>
2018-04-12 17:59:51 +0800

    Merge branch 'bug2'
    # Conflicts:
    #	systemspringboot/systemcommon/src/main/java/com/beppe/service/impl/TestServiceImpl.java
    提交了代码

--- systemspringboot/systemcommon/src/main/java/com/beppe/service/impl/TestServiceImpl.java
+++ systemspringboot/systemcommon/src/main/java/com/beppe/service/impl/TestServiceImpl.java
@@ -8,14 +8,6 @@ public class TestServiceImpl {
     public  void test1(){
         System.out.println("this is the test1");
 
-        System.out.println("正在修复紧急bug");
 
-        System.out.println("紧急bug修复完毕");
-
-        System.out.println("this is the master input");
-
-        System.out.println("开始处理正常的bug了");
-
-        System.out.println("正常的bug处理结束了！！");
     }
 }
